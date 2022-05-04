5a226eb0a87b041ffddf8d2e761185ca875ba31c
AndreiKazakov1 <ankazakov1.21@gmail.com>
2022-05-04 17:21:13 +0300

    added new task 3_3

--- Tasks_3/task3_3.py
+++ Tasks_3/task3_3.py
@@ -2,15 +2,18 @@
 from Tasks_3.task3_for_all import numlist
 
 
-
+indexlist = []
 for i in range(len(numlist)):
     print(f'индекс {i} // значение {numlist[i]} ')
 for i in range(1, len(numlist)):
     if i % 7 == 0:
+        indexlist.append(i)
+        numlist.pop(i)
+        numlist.insert(0, 'x')
+print(indexlist)
+print(numlist)
+for i in range(len(numlist)):
+    if numlist[i] == 'x':
         numlist.pop(i)
 print(numlist)
-
-
-
-
 
