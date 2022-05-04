1413a0f62d0c6eb1d04f211c5e8a27aa02f82045
kan22 <ankazakov1.21@gmail.com>
2022-05-04 20:21:51 +0300

    added task 3_7

--- Tasks_3/task3_7.py
+++ Tasks_3/task3_7.py
@@ -3,7 +3,9 @@ from Tasks_3.task3_for_all import numlist
 
 n = min(numlist)
 print(f'минимальное значение  {n}')
+
 count = 0
+count_del = 0
 numlist_len = len(numlist)
 for i in range(len(numlist)):
     if numlist[i] == n:
@@ -14,5 +16,9 @@ if count == len(numlist):
     print('нет такого элемента для удаления')
 while n in numlist:
     numlist.remove(n)
+    count_del += 1
 if numlist_len > len(numlist):
+    print(f'элемент со значением {n} удален ')
     print(f'результат  {numlist}')
+    print(f'количество удаленных элементов  {count_del}')
+
