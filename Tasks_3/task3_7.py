7bdf1ff5b362fd96619591d149082fc63908a202
kan22 <ankazakov1.21@gmail.com>
2022-05-04 20:15:52 +0300

    added task 3_6

--- Tasks_3/task3_7.py
+++ Tasks_3/task3_7.py
@@ -1,9 +1,8 @@
-# Удалить все элементы с заданным значением, если они имеются в списке.
+# Удалить из списка все элементы, совпадающие с его минимальным значением.
 from Tasks_3.task3_for_all import numlist
 
-n = int(input('введите значение для удаления :'))
-
-
+n = min(numlist)
+print(f'минимальное значение  {n}')
 count = 0
 numlist_len = len(numlist)
 for i in range(len(numlist)):
@@ -16,5 +15,4 @@ if count == len(numlist):
 while n in numlist:
     numlist.remove(n)
 if numlist_len > len(numlist):
-    print(f'элемент со значением {n} удален ')
     print(f'результат  {numlist}')
