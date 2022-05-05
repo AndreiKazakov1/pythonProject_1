d82ffe80132a1234212aebbb619d6fab177b3987
AndreiKazakov1 <ankazakov1.21@gmail.com>
2022-05-05 12:55:28 +0300

    added task 3_16

--- Tasks_3/task3_16.py
+++ Tasks_3/task3_16.py
@@ -1,1 +1,20 @@
-# Найти номера минимального и максимального элементов первой половины списка.
+# Найти номера минимального и максимального элементов первой
+# половины списка.
+
+from task3_for_all import numlist
+
+numlist_half = []
+min_max_elem_ind = []
+for i in range(len(numlist) // 2):
+    numlist_half.append(numlist[i])
+for i in range(len(numlist_half)):
+    if numlist_half[i] == min(numlist_half):
+        min_max_elem_ind.append(i)
+    elif numlist_half[i] == max(numlist_half):
+        min_max_elem_ind.append(i)
+print(f'первая половина списка {numlist_half}')
+min_max_elem_ind.reverse()
+print(f'номера (индексы )минимального и максимального элементов '
+      f'первой половины списка соответственно {min_max_elem_ind}')
+
+
