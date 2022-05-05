287d22a210a8afd45cdb668c8682a7dc939186d4
AndreiKazakov1 <ankazakov1.21@gmail.com>
2022-05-05 12:25:37 +0300

    added task 3_15

--- Tasks_3/task3_15.py
+++ Tasks_3/task3_15.py
@@ -1,11 +1,12 @@
-# 3) Удалить элементы, индексы которых кратны 7.
-from Tasks_3.task3_for_all import numlist
+# Удалить элементы, индексы которых кратны 3.
+
+from task3_for_all import numlist
 
 indexlist = []
 for i in range(len(numlist)):
     print(f'индекс {i} // значение {numlist[i]} ')
 for i in range(1, len(numlist)):
-    if i % 7 == 0:
+    if i % 3 == 0:
         indexlist.append(i)
         numlist.pop(i)
         numlist.insert(0, -1)
@@ -14,5 +15,3 @@ print(f'индексы элементов для удаления  {indexlist}')
 while -1 in numlist:
     numlist.remove(-1)
 print(f'результат после удаления {numlist}')
-
-
