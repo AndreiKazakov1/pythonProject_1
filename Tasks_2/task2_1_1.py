a11ca1cf1eb6f5185099f0b573438ea097c987a8
kan22 <ankazakov1.21@gmail.com>
2022-05-05 21:46:34 +0300

    added task 2_1_1

--- Tasks_2/task2_1_1.py
+++ Tasks_2/task2_1_1.py
@@ -1,11 +1,10 @@
 # 1. Шестизначный автобусный билет считается удачным, если сумма
 # его цифр делится на 7. Могут ли два билета подряд быть удачными?
 
-import random
+# билеты идут подряд
 
-# случайная генерация билетов
-ticket_list = [random.randint(100000, 999999) for _ in range(20)]
-print(*ticket_list)
+ticket_list = [i for i in range(100000, 999999)]
+# print(*ticket_list)
 list_of_sumOfelem = []
 for i in range(len(ticket_list)):
     sumOfelem = 0
@@ -13,12 +12,10 @@ for i in range(len(ticket_list)):
         sumOfelem += ticket_list[i] % 10
         ticket_list[i] //= 10
     list_of_sumOfelem.append(sumOfelem)
-print(list_of_sumOfelem)
-try:
-    for i in range(len(list_of_sumOfelem)):
-        if list_of_sumOfelem[i] % 7 == 0 and list_of_sumOfelem[i+1] % 7 == 0:
-            print('yes, it can be')
-            break
-except Exception:
-    print('the next element in list is not exists')
+# print(list_of_sumOfelem)
+
+for i in range(len(list_of_sumOfelem)):
+    if list_of_sumOfelem[i] % 7 == 0 and list_of_sumOfelem[ i +1] % 7 == 0:
+        print('yes, it can be')
+        break
 
